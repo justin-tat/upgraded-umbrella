ada0f585590b8efddd2b402bba01b740130bb073
deleonenriqueta <enriqueta.correa@gmail.com>
2022-01-04 17:38:27 -0800

    progress on Related carousel

---
+++ client/components/Comparison/Related.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ComparisonModal from './ComparisonModal.jsx';
+
+const RelatedItem = (props) => {
+  console.log('Related Item Props: ', props.image);
+  return (
+    <div className='listItem' >
+      <div className='relatedImgBtn'>
+        <button className='modalButton' onClick={props.displayModal} >
+          <img className="far fa-star"></img>
+        </button>
+        <ComparisonModal/>
+        <img className='carouselImg' src={props.image}></img>
+      </div>
+      <div className='category'>{props.category}</div>
+      <div className='productName'>{props.name}</div>
+      <div className='price'>{props.price}</div>
+      <div className='rating'>XXXXX</div>
+    </div>
+  );
+}
+
+const Related = (props) => {
+  console.log('From Related Data: ', props.relatedData);
+  var relatedList = props.relatedData;
+  console.log('RelatedList: ', relatedList);
+  var relatedItems = relatedList.map((item) =>
+    <RelatedItem key={item.id} productId={item.id} displayModal={props.displayModal} category={item.category} name={item.name} price={item.default_price} image={item.productImg}/>
+  );
+  return(
+      <div className='scrollWrapper'>
+        <div id='carousel'>
+          {relatedItems}
+        </div>
+      </div>
+    )
+}
+
+
+export default Related;
