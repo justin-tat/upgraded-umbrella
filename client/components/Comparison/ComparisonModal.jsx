9a5bac4f590f654590eb139711aa8bdfee50e67a
deleonenriqueta <enriqueta.correa@gmail.com>
2022-01-08 18:22:48 -0800

    implemented checks for specific feature

--- client/components/Comparison/ComparisonModal.jsx
+++ client/components/Comparison/ComparisonModal.jsx
@@ -2,36 +2,66 @@ import React from 'react';
 import Check from './ComparisonImages/checkMark.png';
 
 const Feature = (props) => {
+  console.log('Feature Props: ', props);
+  if (props.feature.check === 'right') {
+    console.log('RIGHT CHECK!');
+    var rightCheck = Check;
+  } else if (props.feature.check === 'left') {
+    console.log('LEFT CHECK!');
+    var leftCheck = Check;
+  } else if (props.feature.check === 'both') {
+    console.log('BOTH CHECK!');
+    var rightCheck = Check;
+    var leftCheck = Check;
+  }
   return(
     <div className='featureRow'>
-      <img className='leftCheck'  ></img>
-      <div className='featureName'>{props.item.feature}: {props.item.value} </div>
-      <img className='rightCheck'  ></img>
+      <div className='checkContainer'>
+        <img className='leftCheck' src={leftCheck} ></img>
+      </div>
+      <div className='featureName'>{props.feature.feature}: {props.feature.value} </div>
+      <div className='checkContainer'>
+        <img className='rightCheck' src={rightCheck} ></img>
+      </div>
     </div>
   )
 }
 
 const ComparisonModal = (props) => {
+  var clickedItemList = props.relatedData.features;
+  var currItemList = props.currProductData.features;
   if (!props.show) {
     return null
   }
-  var clickedItemList = props.relatedData.features;
-  var currItemList = props.currProductData.features;
-  var clickedFeatures = clickedItemList.map((item) =>
-    <Feature key={props.relatedData.id} item={item} />
+  var allItemsList = currItemList;
+  allItemsList.forEach(item => item.check = 'left');
+  var holdingArray = [];
+  for (var clickedItem of clickedItemList) {
+    var foundShared = false;
+    for (var allItem of allItemsList) {
+      if (clickedItem.feature === allItem.feature && clickedItem.value === allItem.value) {
+        allItem.check = 'both';
+        foundShared = true;
+      }
+    }
+    if (!foundShared) {
+      clickedItem.check = 'right';
+      holdingArray.push(clickedItem);
+    }
+  }
+  allItemsList = allItemsList.concat(holdingArray);
+  var modalFeatures = allItemsList.map((feature) =>
+    <Feature key={props.relatedData.styleId} feature={feature} />
   );
-  var currFeatures = currItemList.map((item) =>
-    <Feature key={props.currProductData.id} item={item} />
-  )
+
   return (
     <div className='comparisonModal' onClick={props.onClose}>
       <div className='modalContent' >
         <div className='modalHeader'>
-          <h4 className='modalTitle'>Comparing</h4>
+          <p className='modalTitle'>Comparing</p>
         </div>
         <div className='modalBody'>
-          {clickedFeatures}
-          {currFeatures}
+          {modalFeatures}
         </div>
         <div className='modalFooter'>
         </div>
