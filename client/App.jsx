85eea641e338fba413ff2392d54569a2c86680cc
Justin <jtat5912@gmail.com>
2022-01-10 19:58:14 -0800

    Passing productID as a prop to all widgets

--- client/App.jsx
+++ client/App.jsx
@@ -8,15 +8,18 @@ import "./styles.scss";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      id: 59553
+    }
   }
 
   render() {
     return (
       <div>
         <h1>Team Parasol's App</h1>
-        <Overview/>
-        <Comparison/>
-        <Reviews/>
+        <Overview productId={this.state.id}/>
+        <Comparison productId={this.state.id}/>
+        <Reviews productId={this.state.id}/>
       </div>
     )
   }
