9408f912f0940202f9fba9be4a11fbb0d6c58ac7
deleonenriqueta <enriqueta.correa@gmail.com>
2022-01-13 00:08:04 -0800

    updated Ratings data

--- client/components/Comparison/Related.jsx
+++ client/components/Comparison/Related.jsx
@@ -1,73 +1,60 @@
 import React, {useState} from 'react';
 import ComparisonModal from './ComparisonModal.jsx';
-import NoImage from './ComparisonImages/noImage.png';
 import Star from './ComparisonImages/starImg.png';
-import Half from './ComparisonImages/halfStar.png';
+import NoImage from './ComparisonImages/noImage.png';
+import Ratings from './Ratings.jsx';
+import Price from './Price.jsx';
+
 
 const RelatedItem = (props) => {
-  var productData,
-      price,
-      originalPrice,
-      image,
-      ratings,
-      redStyle;
   var [show, setShow] = useState(false);
+  var image = props.image ? props.image : NoImage;
+  var currProductData,
+      relatedProduct,
+      ratings;
 
-  if (!props.image) {
-    image = NoImage;
-  } else {
-    image = props.image;
-  }
-  for (var product of props.relatedData) {
+  for (var product of props.relatedProducts) {
     if (product.id === props.productId) {
-      productData = product;
+      relatedProduct = product;
     }
   }
-  if (productData.salePrice) {
-    price = '$' + productData.salePrice;
-    originalPrice = '$' + productData.originalPrice;
-    redStyle = {color: 'red'};
-  } else {
-    price = '$' + productData.originalPrice;
-  }
-
-  console.log('this is the ratings: ', props.ratings);
 
   return (
     <div className='listItem'>
-      <div className='relatedImgBox' >
-        <img className='starImg' src={Star} onClick={() => setShow(true)}></img>
-        <img className='relatedImg' src={image}></img>
-      </div>
+    <div className='relatedImgBox' >
+      <img className='starImg' src={Star} onClick={() => setShow(true)}></img>
+      <img className='relatedImg' value={props.productId} src={image} onClick={props.changeId} ></img>
+    </div>
       <div className='relatedDetails'>
         <div className='category'>{props.category}</div>
         <div className='productName'>{props.name}</div>
-        <div className='price'>
-          <div className='salePrice' style={redStyle}>{price}</div>
-          <div className='originalPrice'> {originalPrice}</div>
-        </div>
-        <div className='rating'>
-            <img className='ratingStar' src={Half}></img>
-            <img className='ratingStar' src={Half}></img>
-            <img className='ratingStar' src={Half}></img>
-            <img className='ratingStar' src={Half}></img>
-            <img className='ratingStar' src={Half}></img>
-        </div>
+        <Price salePrice={props.salePrice} originalPrice={props.originalPrice} />
+        <Ratings ratings={props.ratings} />
      </div>
       <ComparisonModal  currProductData={props.currProductData}
-                        relatedData={productData} show={show}
-                        onClose={() => setShow(false)} show={show}/>
+                        relatedProduct={relatedProduct}
+                        show={show}
+                        onClose={() => setShow(false)} show={show}
+      />
     </div>
   );
 }
 
 const Related = (props) => {
-  console.log('Related Props: ', props);
-  var relatedItems = (props.relatedData).map((item) =>
-    <RelatedItem  key={item.id} productId={item.id} relatedData={props.relatedData}
-                  displayModal={props.displayModal} category={item.category} name={item.name}
-                  price={item.default_price} image={item.productImg} ratings={item.ratings}
-                  currProductData={props.currProductData} />
+  console.log('RELATED: ', props.relatedProducts);
+  var relatedItems = (props.relatedProducts).map((product) =>
+    <RelatedItem  key = {product.id}
+                  productId = {product.id}
+                  relatedProducts = {props.relatedProducts}
+                  category = {product.category}
+                  name = {product.name}
+                  salePrice = {product.salePrice}
+                  originalPrice = {product.originalPrice}
+                  image = {product.imageUrl}
+                  ratings = {product.ratings}
+                  currProductData = {props.currProductData}
+                  changeId = {() => {console.log(props.id)}}
+    />
   );
   return(
       <div className='scrollWrapper'>
