0baa8104ee4c99ff48b3c071c17019ce73cbc3ca
Justin <jtat5912@gmail.com>
2022-01-19 20:55:53 -0800

    Finished Merging work!

--- client/components/Overview/AddToCart.jsx
+++ client/components/Overview/AddToCart.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 var AddToCart = props => {
     return (
-        <div id="cart" onClick={props.updateCart}>
-            <div className="cartElem">ADD TO CART</div>
-            <div className="cartElem">+</div>
+        <div id="cartAndStar">
+            <div id="cart" onClick={props.updateCart}>
+                <div className="cartElem">ADD TO CART</div>
+                <div className="cartElem">+</div>
+            </div>
+            <img src='./img/notFavoriteStar.png' id="faveStar"></img>
         </div>
     )
 }
+
+
 
 export default AddToCart;
